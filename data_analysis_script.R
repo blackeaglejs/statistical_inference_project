52cacfbd32d211f535b9039ac3385281340c14d8
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-10-25 14:05:13 -0400

    Add initial versions of script and rmd file

---
+++ data_analysis_script.R
@@ -0,0 +1,14 @@
+## Sets working directory.
+setwd("/Users/Zoheb2/dev/datasciencecoursera/statistical_inference_project")
+
+## Load useful libraries. 
+library(knitr)
+library(ggplot2)
+library(dplyr)
+
+## Set lambda as required by the assignment. 
+lamda <- 0.2
+
+## Set number of simulations 
+nosim <- 1000
+
