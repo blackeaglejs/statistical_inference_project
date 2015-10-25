92ef8e6f6ef9976af23c8e665573bd3f2a399b5d
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-10-25 14:58:37 -0400

    simulations run

--- data_analysis_script.R
+++ data_analysis_script.R
@@ -7,8 +7,17 @@ library(ggplot2)
 library(dplyr)
 
 ## Set lambda as required by the assignment. 
-lamda <- 0.2
+lambda <- 0.2
 
 ## Set number of simulations 
 nosim <- 1000
 
+## Set number of samples. 
+n <- 40
+
+## Ensure reproducability.
+set.seed(234234)
+
+## Run simulations
+exponential_distributions <- matrix(rexp(nosim*n, rate=lambda),nosim,n)
+exponential_means <- data.frame(means=apply(exponential_distributions, 1, mean))
