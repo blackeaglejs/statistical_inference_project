f235bcf6fe09a853eed7721dc8353daa6e8c4c6f
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-10-25 17:04:32 -0400

    Final data analysis script for part 1

--- data_analysis_script.R
+++ data_analysis_script.R
@@ -21,3 +21,31 @@ set.seed(234234)
 ## Run simulations
 exponential_distributions <- matrix(rexp(nosim*n, rate=lambda),nosim,n)
 exponential_means <- data.frame(means=apply(exponential_distributions, 1, mean))
+
+## Calculate theoretical mean
+theoretical_mean <- 1/lambda
+
+## Calculate sample mean
+sample_mean <- mean(exponential_means$means)
+
+## Create a histogram.
+hist(exponential_means$means, breaks=50,
+     main="Distribution of exponential means",
+     xlab = "means",
+     ylab = "density")
+abline(v=theoretical_mean,col="red")
+
+## Calculate theoretical standard deviation and variance.
+theoretical_sd <- (1/lambda)/sqrt(n)
+theoretical_var <- theoretical_sd^2
+
+## Calculate sample standard deviation and variance. 
+sample_sd <- sd(exponential_means$means)
+sample_var <- sample_sd^2
+
+## Create plot comparing to normal distribution.
+ggplot(data=exponential_means, aes(x=means)) + 
+      geom_histogram(binwidth=0.1,aes(y=..density..),alpha=0.2) +
+      stat_function(fun = dnorm, arg=list(mean = theoretical_mean, sd = theoretical_sd)
+                    , col = "red") +
+      geom_density(col="blue")
